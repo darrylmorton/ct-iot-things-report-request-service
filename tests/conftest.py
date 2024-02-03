d29241c983f17d315e9e46bb8b07e02f83951e9f
darrylmorton <darryl.j.morton@gmail.com>
2024-02-03 13:47:39 +0000

    cleanup

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,16 +11,3 @@ def aws_credentials():
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
     os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
-
-# @pytest.fixture(scope="function")
-# async def sqs_client(aws_credentials):
-#     with mock_sqs():
-#         client = boto3.client("sqs", region_name="eu-west-2")
-#
-#         queue = client.create_queue(
-#             QueueName=f"{THINGS_REPORT_REQUEST_QUEUE}.fifo",
-#             # Attributes={'DelaySeconds': '5'}
-#         )
-#         log.info(f"**** queue {queue}")
-#
-#         yield queue
